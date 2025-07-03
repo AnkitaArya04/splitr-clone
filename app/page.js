a8c0352a9e71cf14b65defcd4b58f4b1970e137a
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-03 13:00:57 +0530

    app is initialized

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       Subscribe to Ankita
+      <br />
+      <Button variant={'destructive'}>Subscribe</Button>
     </div>
   );
 }
