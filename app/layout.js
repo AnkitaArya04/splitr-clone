2c4c927ef630ebc75ed7d0ef4229e76d0009a09a
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-03 14:04:03 +0530

    header is defined

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 
 const inter = Inter({subsets:["latin"]});
 
@@ -19,8 +20,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className}`}
       >
+        <ConvexClientProvider>
         <Header />
        <main className="min-h-screen"> {children}</main>
+       </ConvexClientProvider>
       </body>
     </html>
   );
