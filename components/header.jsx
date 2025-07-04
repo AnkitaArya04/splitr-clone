5e6c6c86c4357e92911ff6554d02d139ffff4727
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-04 14:26:57 +0530

    header is designed

--- components/header.jsx
+++ components/header.jsx
@@ -3,6 +3,8 @@
 import { SignInButton,SignedOut,SignedIn, SignUpButton, UserButton } from "@clerk/nextjs";
 import { useStoreUser } from '@/hooks/use-store-user';
 import { BarLoader } from 'react-spinners';
+import Link from "next/link";
+import Image from "next/image";
 
 
 const header = () => {
@@ -10,7 +12,15 @@ const header = () => {
   return (
     <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
-      
+      <Link  href="/" className="flex items-center gap-2">
+      <Image
+            src={"/logos/logo.png"}
+            alt="Vehiql Logo"
+            width={200}
+            height={60}
+            className="h-11 w-auto object-contain"
+          />
+      </Link>
       </nav>
             {isLoading && <BarLoader width={"100%"} color="#36d7b7" />}
     </header>
