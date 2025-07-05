45a268a31cac1a8fe9b71f467638a16d069b0dc9
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-05 17:36:31 +0530

    redirect to sign in

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,14 @@ import { Badge } from "@/components/ui/badge";
 export default function Home() {
   return (
     <div className="flex flex-col pt-16">
-      <section>
-        <div>
+      <section className="mt-20 pb-12 space-y-10 md:space-y-15 px-5">
+        <div className="container mx-auto px-4 md:px-6 text-center space-y-6">
           <Badge variant="outline" className="bg-green-100 text-green-700">
             Split expenses. Simplify life.
           </Badge>
+           <h1 className="gradient-title mx-auto max-w-6xl text-4xl font-bold md:text-8xl">
+            The smartest way to split expenses with friends
+          </h1>
         </div>
       </section>
     </div>
