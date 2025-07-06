c0cfbb1af66721b0b39f9fca3a1262ec0d655cbb
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-06 22:13:50 +0530

    binary to decimal

--- convex/contacts.js
+++ convex/contacts.js
@@ -6,5 +6,26 @@ import { internal } from "./_generated/api";
 export const getAllContacts=query({
     handler:async (ctx)=>{
          const currentUser = await ctx.runQuery(internal.users.getCurrentUser);
+
+         const expensesYouPaid = await ctx.db
+      .query("expenses")
+      .withIndex("by_user_and_group", (q) =>
+        q.eq("paidByUserId", currentUser._id).eq("groupId", undefined)
+      )
+      .collect();
+
+      const expensesNotPaidByYou = (
+      await ctx.db
+        .query("expenses")
+        .withIndex("by_group", (q) => q.eq("groupId", undefined)) // only 1‑to‑1
+        .collect()
+    ).filter(
+      (e) =>
+        e.paidByUserId !== currentUser._id &&
+        e.splits.some((s) => s.userId === currentUser._id)
+    );
+
+    const personalExpenses = [...expensesYouPaid, ...expensesNotPaidByYou];
+
     }
 })
