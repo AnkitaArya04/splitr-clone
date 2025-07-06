fa8ace87ab3ab713300ceead84ec81c9b81cd72b
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-06 22:42:12 +0530

    APIS are created

--- app/(main)/contacts/page.jsx
+++ app/(main)/contacts/page.jsx
@@ -1,11 +1,19 @@
 "use client"
-import { useQuery} from 'convex/react'
+
 import { api } from '@/convex/_generated/api'
+import { useConvexQuery } from '@/hooks/use-convex-query'
 import React from 'react'
+import { BarLoader } from 'react-spinners'
 
 const ContactsPage = () => {
-  const data=useQuery(api.contacts.getAllContacts);
-  console.log(data)
+  const {data,isLoading} = useConvexQuery(api.contacts.getAllContacts);
+  if(isLoading){
+    return(
+      <div>
+        <BarLoader width={"100%"} color='#36d77'/>
+      </div>
+    )
+  }
   return (
     <div>
       ContactPage
