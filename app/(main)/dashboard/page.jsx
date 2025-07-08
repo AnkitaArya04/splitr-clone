2a497945759aeede4c55dbdc6cb494cf958ef265
Ankita Arya <ankitaarya1207@gmail.com>
2025-07-08 22:39:03 +0530

    dashoard page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,6 +1,44 @@
-import React from 'react'
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { useConvexQuery } from "@/hooks/use-convex-query";
+import { BarLoader } from "react-spinners";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { PlusCircle, Users, CreditCard, ChevronRight } from "lucide-react";
+import Link from "next/link";
+// import { ExpenseSummary } from "./components/expense-summary";
+// import { BalanceSummary } from "./components/balance-summary";
+// import { GroupList } from "./components/group-list";
 
 const DashboardPage = () => {
+  const { data: balances, isLoading: balancesLoading } = useConvexQuery(
+    api.dashboard.getUserBalances
+  );
+
+  const { data: groups, isLoading: groupsLoading } = useConvexQuery(
+    api.dashboard.getUserGroups
+  );
+
+  const { data: totalSpent, isLoading: totalSpentLoading } = useConvexQuery(
+    api.dashboard.getTotalSpent
+  );
+
+  const { data: monthlySpending, isLoading: monthlySpendingLoading } =
+    useConvexQuery(api.dashboard.getMonthlySpending);
+
+    const isLoading =
+    balancesLoading ||
+    groupsLoading ||
+    totalSpentLoading ||
+    monthlySpendingLoading;
+
   return (
     <div>
       DashboardPage
